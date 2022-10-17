40510f19e8ee89ca3a0968487d9e5cf9e0f3dd1a
Daniel Lakeland <dlakelan@street-artists.org>
2022-10-17 14:46:40 -0700

    Fiddle with the plot to make it more like what Dave asked for.

--- plotirtt.jl
+++ plotirtt.jl
@@ -4,9 +4,26 @@ file = "./testdata.json"
 
 dat = JSON.parsefile(file)
 rts = dat["round_trips"]
-rtdata = [[x["timestamps"]["client"]["send"]["monotonic"], x["delay"]["send"], 
-    x["delay"]["receive"], x["delay"]["rtt"]] for x in rts if haskey(x,"delay") && haskey(x["delay"],"send")]
+rtdata = DataFrame(timestampns=Vector{Union{Int64,Missing}}(),
+    sendns = Vector{Union{Int64,Missing}}(),
+    receivens = Vector{Union{Int64,Missing}}(), 
+    delayns = Vector{Union{Int64,Missing}}())
 
-plot(map(x->x[1]/1e9,rtdata), map(x->x[4]/1e6,rtdata),title="Delays through time",ylab="Delay (ms)",xlab="Send Time (s)", 
-    size =(1500,500))
+for x in rts
+    if haskey(x,"delay") && haskey(x["delay"],"send")
+        push!(rtdata,(timestampns = x["timestamps"]["client"]["send"]["monotonic"], sendns= x["delay"]["send"], 
+        receivens = x["delay"]["receive"], delayns = x["delay"]["rtt"]))
+    else
+        push!(rtdata,(timestampns = x["timestamps"]["client"]["send"]["monotonic"], sendns=missing,receivens= missing, delayns=missing))
+    end
+end
+
+mindelay = minimum(skipmissing(rtdata.delayns))
+
+replace!(x -> ismissing(x) ? -mindelay : x, rtdata.delayns)
+
+
+@df rtdata scatter(:timestampns / 1e9,:delayns ./ 1e6; size =(1500,500),title="Delay Measurements",xlab="Send Time (s)",ylab="Delay (ms)",
+    markersize=0.5,markerstrokewidth=0,
+    bottom_margin=10Plots.mm, left_margin=10Plots.mm)
 
